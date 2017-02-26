985bfc777f2620554f7d8c7f36e3a5c5c64807d3
Sean Livingston <sean.d.livingston@gmail.com>
2017-02-26 15:33:39 -0500

    Guessing game completed.  First Rust program to teach me the basics of the language.  Going to study this to get more of a feel for the language.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -8,14 +9,31 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101); // uses the 'rand' crate
 
-    println!("The secret number is: {}", secret_number);
+    // println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
     
-    let mut guess = String::new(); // this variable is mutable due to the key word 'mut'. Rust variables are immutable by default
+    loop {
+        let mut guess = String::new(); // this variable is mutable due to the key word 'mut'. Rust variables are immutable by default
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
     
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less      => println!("Too small!"),
+            Ordering::Greater   => println!("Too big"),
+            Ordering::Equal     => {
+                println!("You win!");
+                break;
+
+            }
+        }
+    }
 }
